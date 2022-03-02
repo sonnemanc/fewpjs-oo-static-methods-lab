425085cf2544e4022417299bfca43a72ee84ad85
Corban <sonnemanc@gmail.com>
2022-03-02 14:26:33 -0800

    Done with Assignment

--- index.js
+++ index.js
@@ -1,3 +1,26 @@
 class Formatter {
   //add static methods here
+  static capitalize(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }
+
+  static sanitize(string) {
+    return string.replace( /[^A-Za-z0-9 '-]/g, '' )
+  }
+
+  static titleize(string) {
+    let exept = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from']
+    let splitStr = string.split(" ")
+    let titleized = []
+
+    splitStr.forEach(word => {
+      if (exept.includes(word)) {
+        titleized.push(word);}
+        else {
+          titleized.push(this.capitalize(word))
+        }
+    })
+    return this.capitalize(titleized.join(' '))
+  }
+
 }
